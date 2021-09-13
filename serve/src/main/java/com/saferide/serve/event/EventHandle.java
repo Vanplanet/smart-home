1e3444cdae7a9bd2f6c7a75eb8f9eba12d93b471
vanplanet <916505389@qq.com>
2021-09-13 22:47:49 +0800

    event未实例化解决

--- serve/src/main/java/com/saferide/serve/event/EventHandle.java
+++ serve/src/main/java/com/saferide/serve/event/EventHandle.java
@@ -28,7 +28,8 @@ public class EventHandle implements IEvent {
         queue.add(actions);
 
         // 两个线程的线程池
-        //创建一个单个任务的执异步任务e executor = Executors.newSingleThreadExecutor();
+        //创建一个单个任务的异步任务
+        ExecutorService executor = Executors.newSingleThreadExecutor();
         // 异步处理，创建带返回值的
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
             // 加锁保证事件有序执行
@@ -37,6 +38,7 @@ public class EventHandle implements IEvent {
             }
             return "consumer event end";
         }, executor).exceptionally(e -> e.toString());
+        
         future.whenComplete((r,e) -> {
             log.info(r);
             e.printStackTrace();
